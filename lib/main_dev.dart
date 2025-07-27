5c54ce04a7077c23e3fa4a7609873feefab3e1d7
Taweewong Tocharoen <oat_taweewong@hotmail.com>
2025-07-27 15:34:54 +0700

    Test firebase by calling firestore

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:ours/initializer.dart';
 
 import 'app.dart';
 
 Future main() async {
+  initialize();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(OursApp(flavor: 'development'));
 }
