19e1d218def9f9ef166b893feda591641ceaa42d
Taweewong Tocharoen <oat_taweewong@hotmail.com>
2025-08-10 16:48:58 +0700

    Add theme and resources

---
+++ lib/presentation/resource/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class OurColors {
+  // Light
+  static const lightPrimary = Color(0xFFFFFFFF);
+  static const lightSecondary = Color(0xFF000000);
+  static const lightAccent = Color(0xFFF6872B);
+
+  // Dark
+  static const darkPrimary = Color(0xFF000000);
+  static const darkSecondary = Color(0xFFFFFFFF);
+  static const darkAccent = Color(0xFFAF91E8);
+}
---
+++ lib/presentation/resource/dimens.dart
@@ -0,0 +1,19 @@
+class OurDimension {
+  static Padding padding = Padding();
+  static FontSize fontSize = FontSize();
+}
+
+class Padding {
+  int small = 4;
+  int medium = 8;
+  int large = 16;
+}
+
+class FontSize {
+  double small = 8;
+  double smallMedium = 10;
+  double medium = 12;
+  double mediumLarge = 16;
+  double large = 20;
+  double extraLarge = 28;
+}
