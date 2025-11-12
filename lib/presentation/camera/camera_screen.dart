6b710603711ff9ff1f051da0ca8fef7819abd211
Taweewong Tocharoen <oat_taweewong@hotmail.com>
2025-11-13 00:16:32 +0700

    Add logic to save video

--- lib/presentation/camera/camera_screen.dart
+++ lib/presentation/camera/camera_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 class CameraScreen extends ConsumerStatefulWidget {
   const CameraScreen({super.key});
@@ -14,6 +19,8 @@ class _CameraScreenState extends ConsumerState<CameraScreen> {
   late List<CameraDescription> _cameras;
   late CameraController controller;
   bool _isCameraInitialized = false;
+
+  XFile? videoFile;
 
   @override
   void initState() {
@@ -56,14 +63,24 @@ class _CameraScreenState extends ConsumerState<CameraScreen> {
             child: Center(
               child: FloatingActionButton(
                 onPressed: () {
-                  // TODO: Implement video recording
+                  if (controller.value.isRecordingVideo) {
+                    onStopButtonPressed();
+                  } else {
+                    onVideoRecordButtonPressed();
+                  }
                 },
-                backgroundColor: Colors.red,
-                child: const Icon(Icons.videocam),
+                backgroundColor:
+                    controller.value.isRecordingVideo
+                        ? Colors.red
+                        : Colors.grey,
+                child:
+                    controller.value.isRecordingVideo
+                        ? Icon(Icons.stop)
+                        : Icon(Icons.videocam),
               ),
             ),
           ),
-        ]
+        ],
       ),
     );
   }
@@ -72,5 +89,105 @@ class _CameraScreenState extends ConsumerState<CameraScreen> {
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+
+  void onVideoRecordButtonPressed() {
+    startVideoRecording().then((_) {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  void onStopButtonPressed() {
+    stopVideoRecording().then((XFile? file) {
+      if (mounted) {
+        setState(() {});
+      }
+      if (file != null) {
+        showInSnackBar('Video recorded to ${file.path}');
+        videoFile = file;
+        _saveVideo();
+        // TODO: _startVideoPlayer();
+      }
+    });
+  }
+
+  Future<void> startVideoRecording() async {
+    final CameraController cameraController = controller;
+
+    if (!cameraController.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return;
+    }
+
+    if (cameraController.value.isRecordingVideo) {
+      // A recording is already started, do nothing.
+      return;
+    }
+
+    try {
+      await cameraController.startVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return;
+    }
+  }
+
+  Future<XFile?> stopVideoRecording() async {
+    final CameraController cameraController = controller;
+
+    if (!cameraController.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      return cameraController.stopVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+  }
+
+  Future<void> _saveVideo() async {
+    XFile? video = videoFile;
+    if (video == null) {
+      return;
+    }
+    try {
+      await _saveFileToInternalStorage(videoFile!);
+      showInSnackBar('Video saved!');
+    } catch (e) {
+      showInSnackBar('Error saving video: $e');
+      print("DEBUG_CAMERA: Error saving video: $e");
+    }
+  }
+
+  Future<File> _saveFileToInternalStorage(XFile xfile) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final targetPath = p.join(directory.path, 'parking_record.mp4');
+
+    final targetFile = File(targetPath);
+
+    // Delete existing file if it exists (to overwrite)
+    if (await targetFile.exists()) {
+      await targetFile.delete();
+    }
+
+    // Copy the recorded video to the target path
+    final sourceFile = File(xfile.path);
+    await sourceFile.copy(targetPath);
+
+    return targetFile;
+  }
+
+  void _showCameraException(CameraException e) {
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
   }
 }
